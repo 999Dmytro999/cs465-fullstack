dfaf39b2afb5a7c0bc3caf38ecc1d3e4e3a6989c
Dmytro Sishchuk <dmytro.diia.java@gmail.com>
2026-07-21 12:44:56 -0500

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,15 @@
+var fs = require('fs');
+
+var trips = JSON.parse(
+  fs.readFileSync('./data/trips.json', 'utf8')
+);
+
 var travel = function(req, res) {
-  res.render('travel', { layout: false });
+  res.render('travel', {
+    title: 'Dive Sites - Bhaccasyoniztas Beach Resort Website Template',
+    trips: trips,
+    layout: false
+  });
 };
 
 module.exports = {
